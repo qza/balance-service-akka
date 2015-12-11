a26328313f9a7c2b84a60a802732bc04e599e9ee
qza <kokovic.zoran@gmail.com>
2015-12-11 21:28:11 +0100

    removed request logging, moved to debug level global

--- src/main/scala/org/qza/bs/health/HealthRoutes.scala
+++ src/main/scala/org/qza/bs/health/HealthRoutes.scala
@@ -1,11 +1,10 @@
 package org.qza.bs.health
 
-import akka.event.Logging._
 import akka.http.scaladsl.server.Directives._
 
 trait HealthRoutes extends HealthResponseJson {
 
-  val healthRoutes = logRequestResult("balance-service-health", InfoLevel) {
+  val healthRoutes = {
     path("health") {
       get {
         complete(HealthResponse("ok"))
